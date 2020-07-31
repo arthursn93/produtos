d766741ac2528fe2b6019969c8172bf340105505
Arthur da Silva Nunes <arthurnunes@brq.com>
2020-07-31 18:40:24 -0300

    Atualizando pom.xml

--- src/test/java/br/com/alura/maven/ProdutoTest.java
+++ src/test/java/br/com/alura/maven/ProdutoTest.java
@@ -9,6 +9,9 @@ public class ProdutoTest {
 	@Test
 	public void verificaPrecoComImposto() {
 		Produto bala = new Produto("bala juquinha", 0.10);
-		assertEquals(0.11, bala.getPrecoComImposto(), 0.0001); //valor esperado (com imposto), valor atual, margem de erro
+		assertEquals(0.11, bala.getPrecoComImposto(), 0.0001); 
+		Produto salgadinho = new Produto("salgadinho fandangos", 1.32);
+		assertEquals(1.452, salgadinho.getPrecoComImposto(), 0.0001);
+		//valor esperado (com imposto), valor atual, margem de erro
 	}
 }
